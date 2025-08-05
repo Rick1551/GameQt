e7c799034412bd466078525343128c7f5e903c01
Ricardo Saldaña <ricardo_scuentas@outlook.com>
2025-08-04 19:31:56 -0500

    logica clase controller

--- Controller.h
+++ Controller.h
@@ -3,9 +3,42 @@
 
 #include <QObject>
 
-class Controller
+class Controller : public QObject
 {
     Q_OBJECT
+
+    //Propiedades a guardar/actualizar para conexion entre front-end y back-end
+    Q_PROPERTY(double x READ x WRITE setX NOTIFY xChanged)
+    Q_PROPERTY(double y READ y WRITE setY NOTIFY yChanged)
+
+    //Getters
+    double x(){ return m_x; }
+    double y(){ return m_y; }
+
+    //Setters
+    void setX(double value) {
+        if(m_x != value) {
+            m_x = value;
+            emit xChanged();
+        }
+    }
+
+    void setY(double value) {
+        if(m_y != value){
+            m_y = value;
+            emit yChanged();
+        }
+    }
+
+signals:
+    void xChanged();
+    void yChanged();
+
+private:
+    double m_x; //Current position of our rect on x dimension
+    double m_y; //y position
+    double xSpeed;
+
 public:
     Controller();
 };
